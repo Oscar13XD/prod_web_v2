06e8c76715a6a28c11f78ef6676e3118d7ff5aab
Oscar13XD <hernandezmahechaoscar0@gmail.com>
2024-10-18 15:51:58 -0500

    observacion

---
+++ Scripts SQL/InsercionObservacion/InsercionObservacion.sql
@@ -0,0 +1,20 @@
+INSERT INTO
+    "Configuracion"."Observacion" (
+        "ObsId",
+        "ObsIdUsuario",
+        "ObsDescripcion",
+        "ObsRelevancia",
+        "ObsIdUps",
+        "ObsDtModificacion",
+        "ObsFecha"
+    )
+VALUES
+    (
+        '25fceea1-4008-4528-ba68-fb6fe5c22b7c',
+        '9e4af82e-f96e-4899-94bf-5727710b9854',
+        'Funcional',
+        '5',
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        '12:00:00',
+        '2024-10-18 15:50:00.000'
+    );
