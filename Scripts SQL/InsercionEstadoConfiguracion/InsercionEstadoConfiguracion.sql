e4c58d020620cc2ee5501b87d07ca9ef0aeb1d15
Oscar13XD <hernandezmahechaoscar0@gmail.com>
2024-10-18 15:43:27 -0500

    c

---
+++ Scripts SQL/InsercionEstadoConfiguracion/InsercionEstadoConfiguracion.sql
@@ -0,0 +1,18 @@
+INSERT INTO
+    "Configuracion"."EstadoConfiguracion" (
+        "EtdCfgId",
+        "EtdCfgNombre",
+        "EtdCfgObsId",
+        "EtdCfgUsrId",
+        "EtdCfgTabla",
+        "EtdCfgDtModificacion"
+    )
+VALUES
+    (
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        'Activo',
+        '25fceea1-4008-4528-ba68-fb6fe5c22b7c',
+        NULL,
+        NULL,
+        NULL
+    );
