cdf9f90d031e034ea1fd4b2649cf203ccbb9e45a
Oscar13XD <hernandezmahechaoscar0@gmail.com>
2024-10-24 17:16:27 -0500

    cultivo

---
+++ Scripts SQL/InsercionCultivo/InsercionCultivo1.sql
@@ -0,0 +1,28 @@
+INSERT INTO
+    "Cultivos"."Cultivo" (
+        "ClvId",
+        "ClvTpcId",
+        "ClvPltId",
+        "ClvFchInicio",
+        "ClvCtdInicial",
+        "ClvFchFin",
+        "ClvCtdFinal",
+        "ClvUpsId",
+        "ClvUsrId",
+        "ClvStdId",
+        "ClvDtUtmModificacion"
+    )
+VALUES
+    (
+        '3fff01fb-3dbe-49f5-a1b6-2bdcf8ef39a7',
+        'd5081a23-69e0-4337-8f64-914513aed673',
+        'b14338d1-9dae-4853-9104-55e5f968d65c',
+        '2024-10-24 15:00:00.000',
+        0,
+        '2025-10-24 15:00:00.000',
+        0,
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        '9e4af82e-f96e-4899-94bf-5727710b9854',
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        '2024-10-24 15:00:00.000'
+    );
