cc154fb2acbc7ea9e09b46fad8a4a0bf19ca90ce
Oscar13XD <hernandezmahechaoscar0@gmail.com>
2024-10-24 16:44:36 -0500

    sensor

--- Scripts SQL/InsercionPlantas/InsercionPlanta1.sql
+++ Scripts SQL/InsercionPlantas/InsercionPlanta1.sql
@@ -1,3 +1,16 @@
-INSERT INTO "Cultivos"."Plantas"
-("PltId", "PltNombre", "PltUsrId", "PltStdId", "PltDtUtmModificacion")
-VALUES('b14338d1-9dae-4853-9104-55e5f968d65c', 'Planta 1', '9e4af82e-f96e-4899-94bf-5727710b9854', '3fa85f64-5717-4562-b3fc-2c963f66afa6', '2024-10-18 15:30:00.000');
+INSERT INTO
+    "Cultivos"."Plantas" (
+        "PltId",
+        "PltNombre",
+        "PltUsrId",
+        "PltStdId",
+        "PltDtUtmModificacion"
+    )
+VALUES
+    (
+        'b14338d1-9dae-4853-9104-55e5f968d65c',
+        'Planta 1',
+        '9e4af82e-f96e-4899-94bf-5727710b9854',
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        '2024-10-18 15:30:00.000'
+    );
