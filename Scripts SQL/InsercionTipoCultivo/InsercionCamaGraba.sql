e721600a10f74149412f83bf23ec7c6e5b37c4ef
Oscar13XD <hernandezmahechaoscar0@gmail.com>
2024-10-24 17:08:08 -0500

    tipo de cultivo

---
+++ Scripts SQL/InsercionTipoCultivo/InsercionCamaGraba.sql
@@ -0,0 +1,18 @@
+INSERT INTO
+    "Cultivos"."TipoCultivo" (
+        "TipCltId",
+        "TipCltNombre",
+        "TipCltDescripcion",
+        "TipCltUsrId",
+        "TipCltDtUtmModificacion",
+        "TipCltStdCultivo"
+    )
+VALUES
+    (
+        'd5081a23-69e0-4337-8f64-914513aed673',
+        'Cama de graba',
+        'Cama de graba',
+        '9e4af82e-f96e-4899-94bf-5727710b9854',
+        '2024-10-24 17:00:00.000',
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+    );
