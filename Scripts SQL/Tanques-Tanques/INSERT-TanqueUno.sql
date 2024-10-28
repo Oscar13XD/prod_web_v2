f62d2c4a8605611ec5a5723049fc42b6453bf107
Oscar13XD <hernandezmahechaoscar0@gmail.com>
2024-10-28 12:41:01 -0500

    tanques

---
+++ Scripts SQL/Tanques-Tanques/INSERT-TanqueUno.sql
@@ -0,0 +1,18 @@
+INSERT INTO
+    "Tanques"."Tanques" (
+        "TqsId",
+        "TqsUpsId",
+        "TqsUsrId",
+        "TqsStdId",
+        "TqsDtUtmModificacion",
+        "TqsRtuId"
+    )
+VALUES
+    (
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        '9e4af82e-f96e-4899-94bf-5727710b9854',
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        '2024-08-22 17:30:00.000000',
+        1
+    );
