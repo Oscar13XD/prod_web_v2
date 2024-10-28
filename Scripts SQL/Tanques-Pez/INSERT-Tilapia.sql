47e703fe499da5b6fe3e6288181e8960a77e75b0
Oscar13XD <hernandezmahechaoscar0@gmail.com>
2024-10-28 12:14:03 -0500

    peces

---
+++ Scripts SQL/Tanques-Pez/INSERT-Tilapia.sql
@@ -0,0 +1,16 @@
+INSERT INTO
+    "Tanques"."Pez" (
+        "PezId",
+        "PezNombre",
+        "PezStdId",
+        "PezDtUtmModificacion",
+        "PezUsrId"
+    )
+VALUES
+    (
+        'a7e6b4d9-fc78-459e-8534-3ce3a3c017b6',
+        'Tilapia',
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        '2024-10-28 12:00:00.000',
+        '9e4af82e-f96e-4899-94bf-5727710b9854'
+    );
---
+++ Scripts SQL/Tanques-Pez/INSERT-Trucha.sql
@@ -0,0 +1,16 @@
+INSERT INTO
+    "Tanques"."Pez" (
+        "PezId",
+        "PezNombre",
+        "PezStdId",
+        "PezDtUtmModificacion",
+        "PezUsrId"
+    )
+VALUES
+    (
+        '4bd642fd-9f59-4548-b4c8-2b62b1ceade5',
+        'Trucha',
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        '2024-10-28 12:00:00.000',
+        '9e4af82e-f96e-4899-94bf-5727710b9854'
+    );
