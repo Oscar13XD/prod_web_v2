8eae686022c40303a360373e20962f7335e33b3d
Oscar13XD <hernandezmahechaoscar0@gmail.com>
2024-10-28 08:03:21 -0500

    tomate chonto

---
+++ Scripts SQL/Cultivos-Plantas/INSERT_TomateChonto.sql
@@ -0,0 +1,16 @@
+INSERT INTO
+    "Cultivos"."Plantas" (
+        "PltId",
+        "PltNombre",
+        "PltUsrId",
+        "PltStdId",
+        "PltDtUtmModificacion"
+    )
+VALUES
+    (
+        '35caa2ad-9d3d-4294-8363-9c4a8d381108',
+        'Tomate chonto',
+        '9e4af82e-f96e-4899-94bf-5727710b9854',
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        '2024-10-18 15:30:00.000'
+    );
