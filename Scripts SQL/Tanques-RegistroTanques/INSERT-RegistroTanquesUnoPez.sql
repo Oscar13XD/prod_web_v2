0c13bf57a03662daa63535135709abb15d5dd68f
Oscar13XD <hernandezmahechaoscar0@gmail.com>
2024-10-28 12:49:47 -0500

    registro tanques

---
+++ Scripts SQL/Tanques-RegistroTanques/INSERT-RegistroTanquesUnoPez.sql
@@ -0,0 +1,35 @@
+INSERT INTO
+    "Tanques"."Registro Tanques" (
+        "RgsTqsId",
+        "RgsTqsTqsId",
+        "RgsTqsIdPez",
+        "RgsTqsFchIngreso",
+        "RgsTqsCtdInicial",
+        "RgsTqsFchSalida",
+        "RgsTqsCtdFinal",
+        "RgsTqsDtUtmModificacion",
+        "RqsTqsUsrId"
+    )
+VALUES
+    (
+        uuid_generate_v4 (),
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        '4bd642fd-9f59-4548-b4c8-2b62b1ceade5',
+        '2024-10-28 13:00:00.000000',
+        0,
+        '2024-10-28 13:00:00.000000',
+        0,
+        '2024-10-28 13:00:00.000000',
+        '9e4af82e-f96e-4899-94bf-5727710b9854'
+    ),
+    (
+        uuid_generate_v4 (),
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        'a7e6b4d9-fc78-459e-8534-3ce3a3c017b6',
+        '2024-10-28 13:00:00.000000',
+        0,
+        '2024-10-28 13:00:00.000000',
+        0,
+        '2024-10-28 13:00:00.000000',
+        '9e4af82e-f96e-4899-94bf-5727710b9854'
+    );
