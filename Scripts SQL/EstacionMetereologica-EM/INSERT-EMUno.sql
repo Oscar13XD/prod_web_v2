88d6f3929259b85d35a2591c661c0cffc0dd917a
Oscar13XD <hernandezmahechaoscar0@gmail.com>
2024-10-28 19:46:01 -0500

    estacion metereologica

---
+++ Scripts SQL/EstacionMetereologica-EM/INSERT-EMUno.sql
@@ -0,0 +1,18 @@
+INSERT INTO
+    "EstacionMeteorologica"."EstacionMeteorologica" (
+        "EtMtgaId",
+        "EtMtgaUbicacion",
+        "EtMtgaUpsId",
+        "EtMtgaUsrId",
+        "EtMtgaStdId",
+        "EtMtgaDtUtmModificacion"
+    )
+VALUES
+    (
+        'cfd6cbca-0a29-44ca-afaf-446e225dd806',
+        'Lestoma',
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        '9e4af82e-f96e-4899-94bf-5727710b9854',
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        '2024-10-28 19:45:00.000'
+    );
