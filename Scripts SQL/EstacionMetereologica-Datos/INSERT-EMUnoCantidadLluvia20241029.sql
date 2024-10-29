d529997203ab734d016a99b47d2f7a9d2ab12edb
Oscar13XD <hernandezmahechaoscar0@gmail.com>
2024-10-28 20:02:32 -0500

    estacion metereologica cantidad lluvia

---
+++ Scripts SQL/EstacionMetereologica-Datos/INSERT-EMUnoCantidadLluvia20241029.sql
@@ -0,0 +1,177 @@
+INSERT INTO
+    "EstacionMeteorologica"."DatosEstacionMeteorologica" (
+        "DtemId",
+        "DtemEtMgald",
+        "DtemFchHra",
+        "DtemSnsId",
+        "DtemValor"
+    )
+VALUES
+    (
+        uuid_generate_v4 (),
+        'cfd6cbca-0a29-44ca-afaf-446e225dd806',
+        '2024-10-29 00:00:00.00000',
+        'c5790fcc-ab5c-488a-8441-4d249d6f651b',
+        FLOOR(RANDOM () * (20 - 0 + 1) + 0)
+    ),
+    (
+        uuid_generate_v4 (),
+        'cfd6cbca-0a29-44ca-afaf-446e225dd806',
+        '2024-10-29 01:00:00.00000',
+        'c5790fcc-ab5c-488a-8441-4d249d6f651b',
+        FLOOR(RANDOM () * (20 - 0 + 1) + 0)
+    ),
+    (
+        uuid_generate_v4 (),
+        'cfd6cbca-0a29-44ca-afaf-446e225dd806',
+        '2024-10-29 02:00:00.00000',
+        'c5790fcc-ab5c-488a-8441-4d249d6f651b',
+        FLOOR(RANDOM () * (20 - 0 + 1) + 0)
+    ),
+    (
+        uuid_generate_v4 (),
+        'cfd6cbca-0a29-44ca-afaf-446e225dd806',
+        '2024-10-29 03:00:00.00000',
+        'c5790fcc-ab5c-488a-8441-4d249d6f651b',
+        FLOOR(RANDOM () * (20 - 0 + 1) + 0)
+    ),
+    (
+        uuid_generate_v4 (),
+        'cfd6cbca-0a29-44ca-afaf-446e225dd806',
+        '2024-10-29 04:00:00.00000',
+        'c5790fcc-ab5c-488a-8441-4d249d6f651b',
+        FLOOR(RANDOM () * (20 - 0 + 1) + 0)
+    ),
+    (
+        uuid_generate_v4 (),
+        'cfd6cbca-0a29-44ca-afaf-446e225dd806',
+        '2024-10-29 05:00:00.00000',
+        'c5790fcc-ab5c-488a-8441-4d249d6f651b',
+        FLOOR(RANDOM () * (20 - 0 + 1) + 0)
+    ),
+    (
+        uuid_generate_v4 (),
+        'cfd6cbca-0a29-44ca-afaf-446e225dd806',
+        '2024-10-29 06:00:00.00000',
+        'c5790fcc-ab5c-488a-8441-4d249d6f651b',
+        FLOOR(RANDOM () * (20 - 0 + 1) + 0)
+    ),
+    (
+        uuid_generate_v4 (),
+        'cfd6cbca-0a29-44ca-afaf-446e225dd806',
+        '2024-10-29 07:00:00.00000',
+        'c5790fcc-ab5c-488a-8441-4d249d6f651b',
+        FLOOR(RANDOM () * (20 - 0 + 1) + 0)
+    ),
+    (
+        uuid_generate_v4 (),
+        'cfd6cbca-0a29-44ca-afaf-446e225dd806',
+        '2024-10-29 08:00:00.00000',
+        'c5790fcc-ab5c-488a-8441-4d249d6f651b',
+        FLOOR(RANDOM () * (20 - 0 + 1) + 0)
+    ),
+    (
+        uuid_generate_v4 (),
+        'cfd6cbca-0a29-44ca-afaf-446e225dd806',
+        '2024-10-29 09:00:00.00000',
+        'c5790fcc-ab5c-488a-8441-4d249d6f651b',
+        FLOOR(RANDOM () * (20 - 0 + 1) + 0)
+    ),
+    (
+        uuid_generate_v4 (),
+        'cfd6cbca-0a29-44ca-afaf-446e225dd806',
+        '2024-10-29 10:00:00.00000',
+        'c5790fcc-ab5c-488a-8441-4d249d6f651b',
+        FLOOR(RANDOM () * (20 - 0 + 1) + 0)
+    ),
+    (
+        uuid_generate_v4 (),
+        'cfd6cbca-0a29-44ca-afaf-446e225dd806',
+        '2024-10-29 11:00:00.00000',
+        'c5790fcc-ab5c-488a-8441-4d249d6f651b',
+        FLOOR(RANDOM () * (20 - 0 + 1) + 0)
+    ),
+    (
+        uuid_generate_v4 (),
+        'cfd6cbca-0a29-44ca-afaf-446e225dd806',
+        '2024-10-29 12:00:00.00000',
+        'c5790fcc-ab5c-488a-8441-4d249d6f651b',
+        FLOOR(RANDOM () * (20 - 0 + 1) + 0)
+    ),
+    (
+        uuid_generate_v4 (),
+        'cfd6cbca-0a29-44ca-afaf-446e225dd806',
+        '2024-10-29 13:00:00.00000',
+        'c5790fcc-ab5c-488a-8441-4d249d6f651b',
+        FLOOR(RANDOM () * (20 - 0 + 1) + 0)
+    ),
+    (
+        uuid_generate_v4 (),
+        'cfd6cbca-0a29-44ca-afaf-446e225dd806',
+        '2024-10-29 14:00:00.00000',
+        'c5790fcc-ab5c-488a-8441-4d249d6f651b',
+        FLOOR(RANDOM () * (20 - 0 + 1) + 0)
+    ),
+    (
+        uuid_generate_v4 (),
+        'cfd6cbca-0a29-44ca-afaf-446e225dd806',
+        '2024-10-29 15:00:00.00000',
+        'c5790fcc-ab5c-488a-8441-4d249d6f651b',
+        FLOOR(RANDOM () * (20 - 0 + 1) + 0)
+    ),
+    (
+        uuid_generate_v4 (),
+        'cfd6cbca-0a29-44ca-afaf-446e225dd806',
+        '2024-10-29 16:00:00.00000',
+        'c5790fcc-ab5c-488a-8441-4d249d6f651b',
+        FLOOR(RANDOM () * (20 - 0 + 1) + 0)
+    ),
+    (
+        uuid_generate_v4 (),
+        'cfd6cbca-0a29-44ca-afaf-446e225dd806',
+        '2024-10-29 17:00:00.00000',
+        'c5790fcc-ab5c-488a-8441-4d249d6f651b',
+        FLOOR(RANDOM () * (20 - 0 + 1) + 0)
+    ),
+    (
+        uuid_generate_v4 (),
+        'cfd6cbca-0a29-44ca-afaf-446e225dd806',
+        '2024-10-29 18:00:00.00000',
+        'c5790fcc-ab5c-488a-8441-4d249d6f651b',
+        FLOOR(RANDOM () * (20 - 0 + 1) + 0)
+    ),
+    (
+        uuid_generate_v4 (),
+        'cfd6cbca-0a29-44ca-afaf-446e225dd806',
+        '2024-10-29 19:00:00.00000',
+        'c5790fcc-ab5c-488a-8441-4d249d6f651b',
+        FLOOR(RANDOM () * (20 - 0 + 1) + 0)
+    ),
+    (
+        uuid_generate_v4 (),
+        'cfd6cbca-0a29-44ca-afaf-446e225dd806',
+        '2024-10-29 20:00:00.00000',
+        'c5790fcc-ab5c-488a-8441-4d249d6f651b',
+        FLOOR(RANDOM () * (20 - 0 + 1) + 0)
+    ),
+    (
+        uuid_generate_v4 (),
+        'cfd6cbca-0a29-44ca-afaf-446e225dd806',
+        '2024-10-29 21:00:00.00000',
+        'c5790fcc-ab5c-488a-8441-4d249d6f651b',
+        FLOOR(RANDOM () * (20 - 0 + 1) + 0)
+    ),
+    (
+        uuid_generate_v4 (),
+        'cfd6cbca-0a29-44ca-afaf-446e225dd806',
+        '2024-10-29 22:00:00.00000',
+        'c5790fcc-ab5c-488a-8441-4d249d6f651b',
+        FLOOR(RANDOM () * (20 - 0 + 1) + 0)
+    ),
+    (
+        uuid_generate_v4 (),
+        'cfd6cbca-0a29-44ca-afaf-446e225dd806',
+        '2024-10-29 23:00:00.00000',
+        'c5790fcc-ab5c-488a-8441-4d249d6f651b',
+        FLOOR(RANDOM () * (20 - 0 + 1) + 0)
+    );
