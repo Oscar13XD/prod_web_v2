3d6d07d65d6f4698644eda1a3ec8fbeca7d6ea80
Oscar13XD <hernandezmahechaoscar0@gmail.com>
2024-11-13 15:51:18 -0500

    datos tanques 14 nov

---
+++ Scripts SQL/Tanques-DatosTanques/20241114/INSERT-TanqueUnoConductividadElectrica20241114.sql
@@ -0,0 +1,202 @@
+INSERT INTO
+    "Tanques"."DatosTanque" (
+        "DtqId",
+        "DtqTqsId",
+        "DtqFchHra",
+        "DtqSnsId",
+        "DtqValor",
+        "DtqUpsId"
+    )
+VALUES
+    (
+        uuid_generate_v4 (),
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        '2024-11-14 00:00:00.00000',
+        'd731526e-41f7-4e77-9f8a-26ed2ab260c1',
+        FLOOR(RANDOM () * (100000 - 0 + 1) + 0),
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+    ),
+    (
+        uuid_generate_v4 (),
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        '2024-11-14 01:00:00.00000',
+        'd731526e-41f7-4e77-9f8a-26ed2ab260c1',
+        FLOOR(RANDOM () * (100000 - 0 + 1) + 0),
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+    ),
+    (
+        uuid_generate_v4 (),
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        '2024-11-14 02:00:00.00000',
+        'd731526e-41f7-4e77-9f8a-26ed2ab260c1',
+        FLOOR(RANDOM () * (100000 - 0 + 1) + 0),
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+    ),
+    (
+        uuid_generate_v4 (),
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        '2024-11-14 03:00:00.00000',
+        'd731526e-41f7-4e77-9f8a-26ed2ab260c1',
+        FLOOR(RANDOM () * (100000 - 0 + 1) + 0),
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+    ),
+    (
+        uuid_generate_v4 (),
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        '2024-11-14 04:00:00.00000',
+        'd731526e-41f7-4e77-9f8a-26ed2ab260c1',
+        FLOOR(RANDOM () * (100000 - 0 + 1) + 0),
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+    ),
+    (
+        uuid_generate_v4 (),
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        '2024-11-14 05:00:00.00000',
+        'd731526e-41f7-4e77-9f8a-26ed2ab260c1',
+        FLOOR(RANDOM () * (100000 - 0 + 1) + 0),
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+    ),
+    (
+        uuid_generate_v4 (),
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        '2024-11-14 06:00:00.00000',
+        'd731526e-41f7-4e77-9f8a-26ed2ab260c1',
+        FLOOR(RANDOM () * (100000 - 0 + 1) + 0),
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+    ),
+    (
+        uuid_generate_v4 (),
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        '2024-11-14 07:00:00.00000',
+        'd731526e-41f7-4e77-9f8a-26ed2ab260c1',
+        FLOOR(RANDOM () * (100000 - 0 + 1) + 0),
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+    ),
+    (
+        uuid_generate_v4 (),
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        '2024-11-14 08:00:00.00000',
+        'd731526e-41f7-4e77-9f8a-26ed2ab260c1',
+        FLOOR(RANDOM () * (100000 - 0 + 1) + 0),
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+    ),
+    (
+        uuid_generate_v4 (),
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        '2024-11-14 09:00:00.00000',
+        'd731526e-41f7-4e77-9f8a-26ed2ab260c1',
+        FLOOR(RANDOM () * (100000 - 0 + 1) + 0),
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+    ),
+    (
+        uuid_generate_v4 (),
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        '2024-11-14 10:00:00.00000',
+        'd731526e-41f7-4e77-9f8a-26ed2ab260c1',
+        FLOOR(RANDOM () * (100000 - 0 + 1) + 0),
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+    ),
+    (
+        uuid_generate_v4 (),
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        '2024-11-14 11:00:00.00000',
+        'd731526e-41f7-4e77-9f8a-26ed2ab260c1',
+        FLOOR(RANDOM () * (100000 - 0 + 1) + 0),
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+    ),
+    (
+        uuid_generate_v4 (),
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        '2024-11-14 12:00:00.00000',
+        'd731526e-41f7-4e77-9f8a-26ed2ab260c1',
+        FLOOR(RANDOM () * (100000 - 0 + 1) + 0),
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+    ),
+    (
+        uuid_generate_v4 (),
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        '2024-11-14 13:00:00.00000',
+        'd731526e-41f7-4e77-9f8a-26ed2ab260c1',
+        FLOOR(RANDOM () * (100000 - 0 + 1) + 0),
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+    ),
+    (
+        uuid_generate_v4 (),
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        '2024-11-14 14:00:00.00000',
+        'd731526e-41f7-4e77-9f8a-26ed2ab260c1',
+        FLOOR(RANDOM () * (100000 - 0 + 1) + 0),
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+    ),
+    (
+        uuid_generate_v4 (),
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        '2024-11-14 15:00:00.00000',
+        'd731526e-41f7-4e77-9f8a-26ed2ab260c1',
+        FLOOR(RANDOM () * (100000 - 0 + 1) + 0),
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+    ),
+    (
+        uuid_generate_v4 (),
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        '2024-11-14 16:00:00.00000',
+        'd731526e-41f7-4e77-9f8a-26ed2ab260c1',
+        FLOOR(RANDOM () * (100000 - 0 + 1) + 0),
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+    ),
+    (
+        uuid_generate_v4 (),
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        '2024-11-14 17:00:00.00000',
+        'd731526e-41f7-4e77-9f8a-26ed2ab260c1',
+        FLOOR(RANDOM () * (100000 - 0 + 1) + 0),
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+    ),
+    (
+        uuid_generate_v4 (),
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        '2024-11-14 18:00:00.00000',
+        'd731526e-41f7-4e77-9f8a-26ed2ab260c1',
+        FLOOR(RANDOM () * (100000 - 0 + 1) + 0),
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+    ),
+    (
+        uuid_generate_v4 (),
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        '2024-11-14 19:00:00.00000',
+        'd731526e-41f7-4e77-9f8a-26ed2ab260c1',
+        FLOOR(RANDOM () * (100000 - 0 + 1) + 0),
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+    ),
+    (
+        uuid_generate_v4 (),
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        '2024-11-14 20:00:00.00000',
+        'd731526e-41f7-4e77-9f8a-26ed2ab260c1',
+        FLOOR(RANDOM () * (100000 - 0 + 1) + 0),
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+    ),
+    (
+        uuid_generate_v4 (),
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        '2024-11-14 21:00:00.00000',
+        'd731526e-41f7-4e77-9f8a-26ed2ab260c1',
+        FLOOR(RANDOM () * (100000 - 0 + 1) + 0),
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+    ),
+    (
+        uuid_generate_v4 (),
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        '2024-11-14 22:00:00.00000',
+        'd731526e-41f7-4e77-9f8a-26ed2ab260c1',
+        FLOOR(RANDOM () * (100000 - 0 + 1) + 0),
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+    ),
+    (
+        uuid_generate_v4 (),
+        '40c6e56e-0b90-4143-a4bc-399fdfcdcb45',
+        '2024-11-14 23:00:00.00000',
+        'd731526e-41f7-4e77-9f8a-26ed2ab260c1',
+        FLOOR(RANDOM () * (100000 - 0 + 1) + 0),
+        '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+    );
